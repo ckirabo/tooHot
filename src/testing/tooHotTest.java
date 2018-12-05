75b072c97ac03a2e0ddb875d29f5b64b9247195a
ckirabo <kirabo646@googlemail.com>
2018-12-05 09:38:20 +0000

    Added a test class and a test method that checks if the method tooHot works

--- src/testing/tooHotTest.java
+++ src/testing/tooHotTest.java
@@ -1,5 +1,20 @@
 package testing;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import tooHot.Main;
+
 public class tooHotTest {
 
+	@Test 
+	public void testTooHotMethod() {
+		int a = 10;
+		boolean Summer = false;
+		
+		assertEquals("The actual was not expected", false, Main.tooHot(a,Summer));
+	}
+	
+	
 }
